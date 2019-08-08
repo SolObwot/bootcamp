601b5037729a91981535ac613fa70c69dd6f72bb
Solomon Obwot <solobwot@gmail.com>
2019-08-08 15:26:20 +0300

    editing the factorize file

--- factorize.js
+++ factorize.js
@@ -1,24 +1,3 @@
-// const readline = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// readline.question('Please enter a number to calculate it\s factorial?'),num =>{
-//   if (isNaN(num)) {
-//     console.log('Enter a number')
-//   } else {
-//     console.log(`The factorial of ${num} is ${factorialize(num)}`)
-//   }
-//   readline.close();
-// }
-// const factorialize = function(num) {
-//   if (num === 0) {
-//     return 1;
-//   }
-//   return num * factorialize(num - 1);
-// };
-
-
 function factorialize(num){
     if (num === 0 || num === 1)
     { return 1; }
